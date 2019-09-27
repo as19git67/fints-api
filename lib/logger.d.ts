9c8ee5f229acb91841fabc84d4236981b5931d36
anton <anton@schegg.de>
2019-09-27 08:04:57 +0200

    removed generated *.d.ts files from git

--- lib/logger.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const logger: import("winston").Logger;
-export declare const verbose: (...args: any[]) => any;
-export declare const warn: (...args: any[]) => any;
-export declare const error: (...args: any[]) => any;
--- lib/pain-formats.d.ts
+++
@@ -1,1 +0,0 @@
-export * from "../generated/iso/std/iso/20022/tech/xsd/pain.001.001.03";
