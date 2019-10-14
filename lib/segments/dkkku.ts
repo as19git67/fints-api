7168594756e53dbed3ee6aae91a729406dcd66e7
Anton Schegg <anton@schegg.de>
2019-10-14 17:20:34 +0200

    implement DKKKU segment serialization

--- lib/segments/dkkku.ts
+++ lib/segments/dkkku.ts
@@ -1,42 +1,32 @@
-import { Format } from "../format";
-import { SegmentClass } from "./segment";
-import { SEPAAccount } from "../types";
-import { COUNTRY_CODE } from "../constants";
+import {Format} from "../format";
+import {SegmentClass} from "./segment";
+import {SEPAAccount} from "../types";
+import {COUNTRY_CODE} from "../constants";
 
-export class HKKAZProps {
+export class DKKKUProps {
   public segNo: number;
   public version: number;
   public account: SEPAAccount;
   public startDate: Date;
-  public endDate: Date;
-  public touchdown: string;
 }
 
 /**
  * DKKKU (Kreditkartenumsätze)
- * Section XXX?
  */
-export class DKKKU extends SegmentClass(HKKAZProps) {
+export class DKKKU extends SegmentClass(DKKKUProps) {
   public type = "DKKKU";
 
   protected serialize() {
-    const { version, account, endDate, startDate, touchdown } = this;
-    const { iban, bic, accountNumber, subAccount, blz } = account;
-    if (![4, 5, 6, 7].includes(version)) {
+    const {version, account, startDate} = this;
+    const {iban, bic, accountNumber, subAccount, blz} = account;
+    if (![2].includes(version)) {
       throw new Error(`Unsupported DKKKU version ${version}.`);
     }
-    const serializedAccount = version === 7 ?
-        [iban, bic, accountNumber, subAccount, String(COUNTRY_CODE), blz] :
-        [accountNumber, subAccount, String(COUNTRY_CODE), blz];
-    return [
-      serializedAccount,
-      Format.jn(false),
-      Format.date(startDate),
-      Format.date(endDate),
-      Format.empty(),
-      Format.stringEscaped(touchdown),
-    ];
+    const serializedAccount = [accountNumber, subAccount, String(COUNTRY_CODE), blz];
+    return [serializedAccount, accountNumber, subAccount, Format.date(startDate)];
   }
 
-  protected deserialize() { throw new Error("Not implemented."); }
+  protected deserialize() {
+    throw new Error("Not implemented.");
+  }
 }
